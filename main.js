ebf03297f537c3d27cc26e0b9f1e542de64dcf2b
Ariane Leite <arileitte@gmail.com>
2020-04-03 22:24:29 -0300

    Adicionando regra de fim de jogo

--- main.js
+++ main.js
@@ -26,7 +26,7 @@ function criarCobrinha() {
     }
 }
 
-function drawFood(){
+function drawFood() {
     context.fillStyle = "green";
     context.fillRect(food.x, food.y, box, box);
 }
@@ -46,6 +46,7 @@ function update(event) {
 }
 
 function iniciarJogo() {
+
     if (snake[0].x > 15 * box && direction == "right")
         snake[0].x = 0;
     if (snake[0].x < 0 && direction == "left")
@@ -54,6 +55,13 @@ function iniciarJogo() {
         snake[0].y = 0;
     if (snake[0].y < 0 && direction == "up")
         snake[0].y = 16 * box;
+
+    for (i = 1; i < snake.length; i++) {
+        if (snake[0].x == snake[i].x && snake[0].y == snake[i].y) {
+            clearInterval(jogo);
+            alert('Game Over :(');
+        }
+    }
 
     criarBG();
     criarCobrinha();
@@ -71,7 +79,12 @@ function iniciarJogo() {
     if (direction == "down")
         snakeY += box;
 
-    snake.pop();
+    if (snakeX != food.x || snakeY != food.y) {
+        snake.pop();
+    } else {
+        food.x = Math.floor(Math.random() * 15 + 1) * box,
+            food.y = Math.floor(Math.random() * 15 + 1) * box
+    }
 
     let newHead = {
         x: snakeX,
